bd011a9355d9cbf312d02d772d7f12cc1d7b5e26
Srdjan Misic <srdjan.misic94@gmail.com>
2017-09-21 11:00:46 +0200

    The Webconnection class now properly connects to a website and considers the meta-refresh tag.

--- myWebscraper/src/connection/WebConnection.java
+++ myWebscraper/src/connection/WebConnection.java
@@ -1,61 +1,159 @@
 package connection;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class WebConnection {
-	
+
 	private URL url;
 	private URLConnection con;
+	private boolean hasMetaRefresh = false;
 
-	//public WebConnection() {}
-	public WebConnection(String url)  {
-		
+	public WebConnection(String url) {
+		// assign the passed URL-String to
+		// the url variable
 		try {
-			this.url = new URL(url);
+			// if the url has a meta refresh
+			// the url has to end with a slash
+			// else MalformedURLException
+			this.url = new URL(url.endsWith("/") ? url : url + "/");
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		
+
+		// check if the site is getting redirected
+		// by the meta-refresh tag
 		try {
-			this.con = this.url.openConnection();
+			String refreshLink = getMetaRefresh(this.url.toExternalForm());
+			if (refreshLink != null) {
+				this.url = new URL(refreshLink);
+				this.hasMetaRefresh = true;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	}
-	public WebConnection(URL url)  {
-		this.url = url;
-		
+
+		// open the connection
 		try {
 			this.con = this.url.openConnection();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
-	
+
+	public WebConnection(URL url) {
+		this(url.toExternalForm());
+	}
+
 	public String getHtml() {
 		InputStream is = null;
-		
+
 		try {
 			is = this.con.getInputStream();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		return is != null ? new BufferedReader(new InputStreamReader(is)).lines().parallel().collect(Collectors.joining("\n")) : "";
+
+		return is != null ? new BufferedReader(new InputStreamReader(is)).lines().parallel()
+				.collect(Collectors.joining("\n")).replace("\n", "").replace("\t", "") : "";
+	}
+
+	public URL getUrl() {
+		return this.url;
+	}
+
+	public String getUrlString() {
+		return this.url.toExternalForm();
+	}
+
+	private String getMetaRefresh(String url) throws IOException {
+		if (url != null) {
+			URI uri = URI.create(this.url.toExternalForm());
+			Document d = Jsoup.connect(this.url.toExternalForm()).get();
+
+			for (Element refresh : d.select("meta[http-equiv=Refresh]")) {
+
+				Matcher m = Pattern.compile("(?si)\\d+;\\s*url=(.+)|\\d+").matcher(refresh.attr("content"));
+
+				// find the first one that is valid
+				if (m.matches()) {
+					if (m.group(1) != null)
+						d = Jsoup.connect(uri.resolve(m.group(1)).toString()).get();
+					return d.baseUri();
+				}
+			}
+
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private boolean hasMetaRefresh() {
+		return this.hasMetaRefresh;
 	}
 	
-
-	public static void main(String[] args) {
+	private static void getImage(String src) throws IOException{
+		String folder = null;
+		int indexName=src.lastIndexOf("/");
 		
-		WebConnection wc = new WebConnection("http://www.sandriesser.at/");
-		String html = wc.getHtml();
-		System.out.println(html);
+		if (indexName == src.length()) {
+			src = src.substring(1, indexName);
+		}
+		
+		indexName = src.lastIndexOf("/");
+		String name = src.substring(indexName, src.length());
+		System.out.println(name);
+		
+		URL url = new URL(src);
+		InputStream in = url.openStream();
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(
+				System.getProperty("user.home") + "\\Documents\\test\\"+name));
+		
+		for (int i; (i=in.read()) != -1;) {
+			out.write(i);
+		}
+		out.close();
+		in.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		WebConnection wc = new WebConnection("http://sandriesser.at");
+		System.out.println(wc.getUrlString());
+		// String html = wc.getHtml();
+		// System.out.println(html);
+
+		
+		Response respone = Jsoup.connect(wc.getUrlString()).followRedirects(true).execute();
+		Document doc = Jsoup.connect(respone.url().toExternalForm()).get();
+
+		Elements img = doc.getElementsByTag("img");
+
+		URL nUrl = null;
+		for (Element e : img) {
+			String src = e.absUrl("src");
+			getImage(src);
+			
+		}
+		
+		
 
 	}
 
